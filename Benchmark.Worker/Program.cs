672aa44b75ba279d20dd658c7b8ba2665ead0a10
Marko Preradović <marko.preradovic416@gmail.com>
2025-12-23 15:35:58 +0100

    Fixed ILlmClient and OpenAiClient. Problem: API requests run off easily. Solution: More credits?

--- Benchmark.Worker/Program.cs
+++ Benchmark.Worker/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddHostedService<Worker>();
 builder.Services.AddSingleton<FailureAnalyzer>();
 builder.Services.AddSingleton<DatasetBuilder>();
 builder.Services.AddSingleton<BenchmarkRunner>();
-builder.Services.AddSingleton<ILlmClient>(sp => new OpenAiClient("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"));
+builder.Services.AddSingleton<ILlmClient>(sp => new OpenAiClient("api key"));
 builder.Services.AddSingleton<TrainingOrchestrator>();
 builder.Services.AddSingleton<CodeEvaluator>();
 builder.Services.AddSingleton<CodeSandbox>();
